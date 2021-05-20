6fcf3107d65d0864806f7ae80be19f158b21d4e9
nxtcoder17 <nxtcoder17@gmail.com>
2021-05-20 16:45:48 +0530

    added prettier in config-react

--- packages/eslint-config-react/eslintrc.js
+++ packages/eslint-config-react/eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  extends: ['eslint-config-airbnb', 'plugin:react/recommended'],
-  plugins: ['import', 'react'],
-
   parser: '@babel/eslint-parser',
   parserOptions: {
     babelOptions: {
       configFile: './babel.config.json',
     },
   },
+
+  extends: ['airbnb', 'airbnb/hooks', "plugin:prettier/recommended", 'plugin:react/recommended'],
+  plugins: ['react', 'import', 'prettier'],
+
 
   env: {
     node: true,
@@ -39,7 +40,9 @@ module.exports = {
     'react/jsx-uses-react': 0,
     'react/jsx-uses-vars': 0,
     'react/jsx-props-no-spreading': 0,
+    'react/react-in-jsx-scope': 0,
 
+    "prettier/prettier": ["error", {"singleQuote": true }],
     camelcase: ['error', { ignoreDestructuring: true, properties: 'never' }],
     'no-underscore-dangle': 0,
     'no-unused-vars': [
